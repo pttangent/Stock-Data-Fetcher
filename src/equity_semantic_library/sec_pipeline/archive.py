22f9ddf0a529cfbffa2dafa8455a683efb19c558
POHTHONG <109568219+pttangent@users.noreply.github.com>
2026-07-21 14:40:11 +0800

    Use explicit SEC archive exports

--- src/equity_semantic_library/sec_pipeline/archive.py
+++ src/equity_semantic_library/sec_pipeline/archive.py
@@ -1,2 +1,37 @@
-from .archive_source import *
-from .archive_parse import *
+from .archive_parse import (
+    extract_inline_xbrl,
+    extract_sections,
+    extract_tables,
+    html_to_text,
+    parse_13f,
+    parse_documents,
+    parse_xml,
+)
+from .archive_source import (
+    GlobalRateGate,
+    SecArchive,
+    SecClient,
+    inventory_archive,
+    load_symbol_metadata,
+    read_storage_uri,
+    sha256_bytes,
+    sha256_file,
+)
+
+__all__ = [
+    "GlobalRateGate",
+    "SecArchive",
+    "SecClient",
+    "extract_inline_xbrl",
+    "extract_sections",
+    "extract_tables",
+    "html_to_text",
+    "inventory_archive",
+    "load_symbol_metadata",
+    "parse_13f",
+    "parse_documents",
+    "parse_xml",
+    "read_storage_uri",
+    "sha256_bytes",
+    "sha256_file",
+]
